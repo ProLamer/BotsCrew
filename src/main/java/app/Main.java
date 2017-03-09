79835b5cbb69be77d63084c9d3f4b0f9ee4a9f53
ProLamer <prolamer99lvl@gmail.com>
2017-03-09 20:44:45 +0200

    BotsCrew Forever

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,6 +1,6 @@
 package app;
 
-import controller.controllerImpl.BookController;
+import controller.BookController;
 
 public class Main {
 
--- src/main/java/controller/BookController.java
+++ src/main/java/controller/BookController.java
@@ -1,4 +1,4 @@
-package controller.controllerImpl;
+package controller;
 
 import java.util.Scanner;
 
